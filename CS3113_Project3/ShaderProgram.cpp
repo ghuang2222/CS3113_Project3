ded7a6329e71d7f3e8e263e7b40d0eecdf0d7fb0
ghuang2222 <gh2222@nyu.edu>
2024-07-13 22:06:57 -0400

    added header

--- CS3113_Project3/ShaderProgram.cpp
+++ CS3113_Project3/ShaderProgram.cpp
@@ -1,3 +1,13 @@
+/**
+* Author: Garvin Huang
+* Assignment: Lunar Lander
+* Date due: 2024-07-13, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #define GL_SILENCE_DEPRECATION
 
 #include "ShaderProgram.h"
